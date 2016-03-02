81624632c3257a4d474e300eeec7999d59991a93
bronzon <olle.back@gmail.com>
2016-03-02 21:56:40 +0100

    collision and simplified animations

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	public float speed = 1;
 	private Animator animator;
-	private Vector2 direction = new Vector2(0,0);
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,35 +13,20 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		int horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
-		int vertical = (int) (Input.GetAxisRaw ("Vertical"));
-		if (horizontal > 0) {
-			if (direction.x <= 0) {
-				animator.SetTrigger ("right");
-				direction.x = 1;
-				direction.y = 0;
-			}
-		} else if (horizontal < 0) {
-			if (direction.x >= 0) { 
-				animator.SetTrigger ("left");
-				direction.x = -1;
-				direction.y = 0;
-			}
-		} else if (vertical > 0) {
-			if (direction.y <= 0) {
-				animator.SetTrigger ("up");
-				direction.y = 1;
-				direction.x = 0;
-			}
-		} else if (vertical < 0) {
-			if (direction.y >= 0) {
-				animator.SetTrigger ("down");
-				direction.y = -1;
-				direction.x = 0;
+		Vector2 direction = new Vector2(Input.GetAxisRaw ("Horizontal"),Input.GetAxisRaw ("Vertical"));
+
+		animator.SetInteger ("horizontal", (int)direction.x);
+		animator.SetInteger ("vertical", (int)direction.y);
+
+		if (direction.magnitude > float.Epsilon) {
+			Vector2 estimatedPos = new Vector2(transform.position.x + direction.x*speed, transform.position.y + direction.y*speed);
+			RaycastHit2D hit = Physics2D.Linecast (transform.position, estimatedPos);
+			if (hit.collider == null) {
+				transform.position = estimatedPos;
 			}
 		}
+	
 
-		transform.Translate (direction);
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			animator.SetTrigger ("chop");
